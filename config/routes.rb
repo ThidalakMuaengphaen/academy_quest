de951e323bfb5f8c441a024199d4f79c9017b116
ThidalakMuaengphaen <thidalak.m@kkumail.com>
2025-08-20 17:48:33 +0700

    Fix: restrict quest_lists routes to only necessary actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :quest_lists
+  resources :quest_lists, only: [ :index, :create, :update, :destroy ]
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "quest_lists#index"
   get "/brag" => "brag#index", as: :brag
+
+  get "*path", to: redirect("/")
 end
